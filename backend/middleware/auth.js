2305687c2d209edd7a1b42000745bd64c9fa1d22
lingering126 <renyi7318@gmail.com>
2025-05-22 05:36:09 +0800

    tests

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -6,9 +6,6 @@ const mongoose = require('mongoose');
  * Verifies the JWT token and sets user information in the request object
  */
 const authMiddleware = async (req, res, next) => {
-  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-  console.log('!!!! Auth Middleware IS RUNNING for path:', req.originalUrl, 'method:', req.method);
-  console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
   console.log('==== Auth Middleware START ====');
 
   try {
