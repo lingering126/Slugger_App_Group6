ae9da65ac03a9bd525359f1ab35af207bc01e7a9
May Han <may.zhe.han@gmail.com>
2025-05-26 08:31:23 +0800

    Set paddingHorizontal of actionButton to 6 for better display

--- app/screens/(tabs)/home.jsx
+++ app/screens/(tabs)/home.jsx
@@ -2432,7 +2432,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingVertical: 6, 
-    paddingHorizontal: 8, 
+    paddingHorizontal: 6, 
     borderRadius: 16, 
     minWidth: 80, 
     marginVertical: 3, 
